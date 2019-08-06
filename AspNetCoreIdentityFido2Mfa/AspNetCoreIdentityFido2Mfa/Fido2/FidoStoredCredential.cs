6ad49751e47cda7c899bb50f6fbca951125b7203
damienbod <damien_bod@hotmail.com>
2019-08-06 08:05:09 +0200

    add entity to scripts

--- AspNetCoreIdentityFido2Mfa/AspNetCoreIdentityFido2Mfa/Fido2/FidoStoredCredential.cs
+++ AspNetCoreIdentityFido2Mfa/AspNetCoreIdentityFido2Mfa/Fido2/FidoStoredCredential.cs
@@ -1,17 +1,27 @@
 ﻿using Fido2NetLib.Objects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AspNetCoreIdentityFido2Mfa
 {
     public class FidoStoredCredential
     {
         public byte[] UserId { get; set; }
-        public PublicKeyCredentialDescriptor Descriptor { get; set; }
         public byte[] PublicKey { get; set; }
         public byte[] UserHandle { get; set; }
         public uint SignatureCounter { get; set; }
         public string CredType { get; set; }
         public DateTime RegDate { get; set; }
         public Guid AaGuid { get; set; }
+
+        [NotMapped]
+        public PublicKeyCredentialDescriptor Descriptor
+        {
+            get { return JsonConvert.DeserializeObject<PublicKeyCredentialDescriptor>(DescriptorJson); }
+            set { DescriptorJson = JsonConvert.SerializeObject(value); }
+        }
+        public string DescriptorJson { get; set; }
     }
 }
