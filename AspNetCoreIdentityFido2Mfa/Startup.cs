0e00234879432a873cc473a9ff84cfa4b32c97bd
damienbod <damien_bod@hotmail.com>
2019-10-17 13:15:03 +0200

    updating configuration

--- AspNetCoreIdentityFido2Mfa/Startup.cs
+++ AspNetCoreIdentityFido2Mfa/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Fido2NetLib;
+using Fido2Identity;
 
 namespace AspNetCoreIdentityFido2Mfa
 {
