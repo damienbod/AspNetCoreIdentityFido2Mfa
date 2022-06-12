e5eeecbb379e53fe0959724eb631e675e8fe18b0
damienbod <damien_bod@hotmail.com>
2022-06-12 07:53:30 +0200

    fix RememberMe, ReturnUrl not passed to MFA

--- AspNetCoreIdentityFido2Mfa/Areas/Identity/Pages/Account/LoginFido2Mfa.cshtml.cs
+++ AspNetCoreIdentityFido2Mfa/Areas/Identity/Pages/Account/LoginFido2Mfa.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspNetCoreIdentityFido2Mfa.Areas.Identity.Pages.Account;
@@ -6,8 +7,10 @@ namespace AspNetCoreIdentityFido2Mfa.Areas.Identity.Pages.Account;
 [AllowAnonymous]
 public class MfaModel : PageModel
 {
+    [BindProperty(SupportsGet = true)]
     public bool RememberMe { get; set; }
 
+    [BindProperty(SupportsGet = true)]
     public string? ReturnUrl { get; set; }
 
     public void OnGet()
