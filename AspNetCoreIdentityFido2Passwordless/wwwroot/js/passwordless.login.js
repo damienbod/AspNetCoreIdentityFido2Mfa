2b739352d75afaf8d5dda3e08c6eb6ee0f424331
damienbod <damien_bod@hotmail.com>
2022-06-29 23:11:14 +0200

    remove user handle

--- AspNetCoreIdentityFido2Passwordless/wwwroot/js/passwordless.login.js
+++ AspNetCoreIdentityFido2Passwordless/wwwroot/js/passwordless.login.js
@@ -93,7 +93,7 @@ async function verifyAssertionWithServer(assertedCredential) {
         response: {
             authenticatorData: coerceToBase64Url(authData),
             clientDataJson: coerceToBase64Url(clientDataJSON),
-            userHandle: userHandle !== null ? coerceToBase64Url(userHandle) : null,
+            //userHandle: userHandle !== null ? coerceToBase64Url(userHandle) : null,
             signature: coerceToBase64Url(sig)
         }
     };
